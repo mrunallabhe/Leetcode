7aafadd6fa7c51390ad8824882d41b0b15f695db
mrunallabhe <150068436+mrunallabhe@users.noreply.github.com>
2025-07-24 22:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (88.86%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -10,14 +10,14 @@ class Solution {
             return 0;
         }
 
-        // Recursively compute the maximum sum of the left and right subtree paths.
+        
         int leftSum = Math.max(0, dfs(node.left, res));
         int rightSum = Math.max(0, dfs(node.right, res));
 
-        // Update the maximum path sum encountered so far (with split).
+        
         res[0] = Math.max(res[0], leftSum + rightSum + node.val);
 
-        // Return the maximum sum of the path (without split).
+      
         return Math.max(leftSum, rightSum) + node.val;
     }    
 }
