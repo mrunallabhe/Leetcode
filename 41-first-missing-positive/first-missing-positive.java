c93b7da7aaeb5e2a775072bd594b91e251b091f1
mrunallabhe <150068436+mrunallabhe@users.noreply.github.com>
2025-12-20 09:50:15 +0530

    Time: 19 ms (11.42%) | Memory: 93.3 MB (5.54%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,18 +1,31 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int[] filteredNums = Arrays.stream(nums).filter(n -> n > 0).toArray();
-        
-        Arrays.sort(filteredNums);
-        
-        int target = 1;
-        for (int n : filteredNums) {
-            if (n == target) {
-                target++;
-            } else if (n > target) {
-                return target;
+        HashSet<Integer> set = new HashSet<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        // store positive numbers & track min/max
+        for (int num : nums) {
+            if (num > 0) {
+                set.add(num);
+                min = Math.min(min, num);
+                max = Math.max(max, num);
             }
         }
-        
-        return target;        
+
+        // if 1 is missing
+        if (!set.contains(1)) {
+            return 1;
+        }
+
+        // check missing in range
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+
+        // all present
+        return max + 1;
     }
 }
