c6b715b11650de27fa2a1278184cd84ab0f0c15f
mrunallabhe <150068436+mrunallabhe@users.noreply.github.com>
2026-01-06 22:21:03 +0530

    Time: 1 ms (24.12%) | Memory: 43.3 MB (77.95%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,6 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        int zerocount = map.getOrDefault(0, 0);
+        int onecount  = map.getOrDefault(1, 0);
+        int twocount  = map.getOrDefault(2, 0);
+
+        // Fill 0s
+        for (int i = 0; i < zerocount; i++) {
+            nums[i] = 0;
+        }
+
+        // Fill 1s
+        for (int j = zerocount; j < zerocount + onecount; j++) {
+            nums[j] = 1;
+        }
+
+        // Fill 2s
+        for (int k = zerocount + onecount; k < nums.length; k++) {
+            nums[k] = 2;
+        }
     }
 }
