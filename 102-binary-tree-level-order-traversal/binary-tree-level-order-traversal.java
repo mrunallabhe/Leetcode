77c24b12208a281a4328c0ac4490680db9166aac
mrunallabhe <150068436+mrunallabhe@users.noreply.github.com>
2026-01-08 23:25:37 +0530

    Time: 1 ms (94.86%) | Memory: 46.9 MB (42.50%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,47 +15,22 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
-            // If the tree is empty,
-            // return an empty list
-            return ans;
-        }
 
-        // Create a queue to store nodes
-        // for level-order traversal
+        List<List<Integer>> ans = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
-        // Push the root node to the queue
-        q.add(root);
-
-        while (!q.isEmpty()) {
-            // Get the size of the current level
-            int size = q.size();
-            // Create a list to store
-            // nodes at the current level
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                // Get the front node in the queue
-                TreeNode node = q.poll();
-                // Store the node value
-                // in the current level list
-                level.add(node.val);
-
-                // Enqueue the child nodes if they exist
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right != null) {
-                    q.add(node.right);
-                }
+        if (root == null) return ans;
+        q.offer(root);
+        while (!q.isEmpty()){
+            List<Integer> row = new ArrayList<>();
+            int n = q.size();
+            for (int i =0; i< n ; i++){
+                TreeNode front = q.poll();
+                row.add(front.val);
+                if (front.left != null) q.offer(front.left);
+                if (front.right != null) q.offer(front.right);
             }
-            // Store the current level
-            // in the answer list
-            ans.add(level);
-     
-        } 
+            ans.add(row);
+        }
         return ans;
     }
-    
 }
