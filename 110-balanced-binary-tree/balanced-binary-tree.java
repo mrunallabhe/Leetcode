e13f00504baf50cb31adb14d41d197d791c26aa1
mrunallabhe <150068436+mrunallabhe@users.noreply.github.com>
2026-01-09 17:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (43.04%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,46 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-            return true;
+        if (root == null) return true;
+
+        int ldepth = maxdepth(root.left);
+        int rdepth = maxdepth(root.right);
+
+        if (Math.abs(ldepth - rdepth) > 1) {
+            return false;
         }
 
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
-
-        if (Math.abs(leftHeight - rightHeight) <= 1 &&
-            isBalanced(root.left) &&
-            isBalanced(root.right)) {
-            return true;
-        }
-
-        return false;
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 
-    
-    public int getHeight(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
+    public int maxdepth(TreeNode l) {
+        if (l == null) return 0;
 
-        
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
+     
+        int left = maxdepth(l.left);
+        int right = maxdepth(l.right);
 
-        return Math.max(leftHeight, rightHeight) + 1;
+        return Math.max(left, right) + 1;
     }
-    }
+}
